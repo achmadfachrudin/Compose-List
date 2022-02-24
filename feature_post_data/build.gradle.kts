bfc08a3b8b947e562d3cb649f293d55959174619
achmadfachrudin <a2fachrudin@gmail.com>
2022-02-24 21:39:05 +0700

    update dependency

--- feature_post_data/build.gradle.kts
+++ feature_post_data/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
     id("com.android.library")
     id("kotlin-parcelize")
@@ -28,8 +26,8 @@ android {
 
 dependencies {
     // Moshi
-    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
 
     // Room
     implementation("androidx.room:room-ktx:2.4.1")
